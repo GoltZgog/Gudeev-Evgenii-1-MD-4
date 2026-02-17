0cb5642fe12d6e70348ee90f194a7f7348f6d31d
laberuchi <eg6657844@gmail.com>
2026-02-17 21:58:57 +0300

    v1

---
+++ lab3.py
@@ -0,0 +1,39 @@
+import math
+print("цэ первая часть:S=πr2S")
+r=int(input("mazani: "))
+s=math.pi * r**2
+print(s)
+
+print("цэ вторая часть:ax+b=0")
+r=int(input('mazani: '))
+e=int(input('mazani teper drugoe: '))
+s=((-e)/r)
+print(s)
+
+print("цэ третья часть: цельсий в => фарингейт")
+r=int(input('mazani: '))
+s=r*(9/5) + 32
+print(s)
+
+print("цэ чётвёртая часть: среднее арифметическое")
+r=int(input('mazani: '))
+t=int(input('mazani: '))
+y=int(input('mazani: '))
+s=(r+t+e)/3
+print(s)
+
+print("цэ чётвёртая часть: max значение \n r=5 + 2 * 3 - 4 / 2 \n t=(3 + 5) * (2 + 4) / 2 \n y=-3 + 6 / 2 * 4 \n u=5+4*5**2+7")
+
+r=5 + 2 * 3 - 4 / 2
+t=(3 + 5) * (2 + 4) / 2
+y=-3 + 6 / 2 * 4
+u=5+4*5**2+7
+print("цэ max значением является *барабанная дробь*")
+if r == max(r,t,e,u):
+    print("r: ",r)
+elif t == max(r,t,e,u):
+    print("t: ", t)
+elif y == max(r,t,e,u):
+    print("y: ", y)
+elif u == max(r,t,e,u):
+    print("u: ", u)
